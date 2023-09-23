3ddc67363d7a3bc32eca6a71921ba7255fc4b334
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-23 17:37:52 +0200

    ByRegionPageComponent: optimitation

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interfaces';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -12,8 +14,12 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.isLoading = true;
 
     this.countriesService.searchRegion(region).subscribe((countries) => {
